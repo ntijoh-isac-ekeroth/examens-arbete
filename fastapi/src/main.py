461c18c17792e2cb3f9ffaba7890fc1a59477133
ntijoh-isac-ekeroth <isac.ekeroth@elev.ga.ntig.se>
2023-05-11 15:36:57 +0200

    Add all endpoints to fastapi

--- fastapi/src/main.py
+++ fastapi/src/main.py
@@ -1,8 +1,35 @@
 from fastapi import FastAPI
+from time import sleep
+from pydantic import BaseModel
+
+
+class Post_Read_Body(BaseModel):
+    message: str
+
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/raw_json")
+async def raw_json():
+    return {"message": "Static GET Success"}
+
+@app.get('/parse_url/{id}')
+async def parse_url(id : int):
+    return {'message': id}
+
+@app.post('/delay/{id}')
+async def delay(id : int):
+    sleep(id)
+    return {"message": id}
+
+@app.post('/post')
+async def post():
+    return {"message": "Static POST Success"}
+
+@app.post('/post_read')
+async def post_read(body : Post_Read_Body):
+    return body
